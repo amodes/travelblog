70ee5caa6c825197d4ba65a4e6557be7c22cc36b
Alexander Modes <alex.modes@joindeed.com>
2024-03-08 02:24:22 +0100

    Add imprint to footer

--- src/components/templates/footer/footer.tsx
+++ src/components/templates/footer/footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'next-i18next';
+import Link from 'next/link';
 
 import { Container } from '@src/components/shared/container';
 
@@ -6,10 +7,14 @@ export const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="border-t-color mt-10 border-t border-gray200">
-      <Container className="py-8">
-        <h2 className="h4 mb-4">{t('footer.aboutUs')}</h2>
-        <div className="max-w-4xl">{t('footer.description')}</div>
+    <footer className="border-t-color mt-10 border-t border-gray400">
+      <Container className="flex gap-x-10 py-8">
+        <div>
+          <div className="max-w-4xl">{t('footer.description')}</div>
+        </div>
+        <Link className="flex flex-col" href={`/imprint`}>
+          <div className="max-w-4xl">{t('footer.imprint')}</div>
+        </Link>
       </Container>
     </footer>
   );
