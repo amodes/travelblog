02b7e2a1676c213d31186a237ee763f49c1a4103
Alexander Modes <alex.modes@joindeed.com>
2024-03-13 23:04:41 +0100

    Use Script instead of @next/third-parties

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,5 +1,5 @@
-import { GoogleTagManager } from '@next/third-parties/google';
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -12,12 +12,25 @@ export default function Document() {
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_TAG_MANAGER_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
-      <GoogleTagManager gtmId={`${process.env.GOOGLE_TAG_MANAGER_ID}`} />
     </Html>
   );
 }
