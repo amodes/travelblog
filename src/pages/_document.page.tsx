03e66d2cbbfc7d9d4596865bafdba96cd8ff4894
Alexander Modes <alex.modes@joindeed.com>
2024-11-23 15:10:10 +0100

    Rename environment variable

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -18,7 +18,7 @@ export default function Document() {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
         `}
         </Script>
       </Head>
@@ -27,7 +27,7 @@ export default function Document() {
         <NextScript />
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_TAG_MANAGER_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
           }}
         />
       </body>
